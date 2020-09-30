9ec93b34ea9f02ad4d50845cc98c90e8bde98d52
zamiladhamaranugerahdwiyonarta <zamiladhamar66@gmail.com>
2020-10-01 01:54:45 +0700

    add email

--- email.php
+++ email.php
@@ -1,13 +1,42 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
-    ini_set( 'display_errors', 1 );   
-    error_reporting( E_ALL );    
-    $from = "ekoaewes2006@gmail.com";    
-    $to = "eko.febri81@gmail.com";    
-    $subject = "Checking PHP mail";    
-    $message = "PHP mail berjalan dengan baik";   
-    $headers = "From:" . $from;    
-    mail($to,$subject,$message, $headers);    
-    echo "Pesan email sudah terkirim.";
+require_once "vendor/autoload.php";
 
-?>
+$mail = new PHPMailer(true);
+
+//Enable SMTP debugging.
+$mail->SMTPDebug = 3;                               
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();            
+//Set SMTP host name                          
+$mail->Host = "wb-productions.id";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                          
+//Provide username and password     
+$mail->Username = "admin@wb-productions.id";                 
+$mail->Password = "Wbproductions2020";                           
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                           
+//Set TCP port to connect to
+$mail->Port = 587;                                   
+
+$mail->From = "admin@wb-productions.id";
+$mail->FromName = "Admin WB productions";
+
+$mail->addAddress("zamil@wb-productions.id", "zamil");
+
+$mail->isHTML(true);
+
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
